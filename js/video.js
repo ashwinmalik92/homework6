8c45cb20f3c543af80be45af2f401699df3c0425
Ashwin Malik <ashwinmalik92@gmail.com>
2020-02-22 17:55:07 -0500

    Video updates

--- js/video.js
+++ js/video.js
@@ -1,52 +1,81 @@
 var video;
+var newRate;
+var duration;
+var newTime;
+var dispVolume;
 
-function getVid(){
+function getVid() {
 	video = document.querySelector("#myVideo"); 
+	video.setAttribute("poster", "images/thumb.jpg");
+	document.querySelector("#volume").innerHTML = "100%";
 }
 
 function playVid() { 
-
+	video.play();
 	console.log("Play Video");
 } 
 
 function pauseVid() { 
-
+	video.pause();
 	console.log("Pause Video");
-} 
+}
 
-function decreaseSpeed() { 
-
-  	console.log("Speed is "+ );
-} 
+function decreaseSpeed() {
+	newRate = video.playbackRate * .8;
+	if (newRate < 0.1) {
+		newRate = 0.1073741824;
+	}
+	video.playbackRate = newRate;
+	console.log("Speed is " + newRate);
+}
 
 function increaseSpeed() {
-
-	console.log("Speed is "+ );
+	video.playbackRate = video.playbackRate * 1.25;
+	console.log("Speed is " + video.playbackRate);
 } 
 
 function skipAhead() {
-
-	console.log("Current location is "+ );
+	duration = video.duration;
+	newTime = video.currentTime + 60;
+	if (newTime > duration) {
+		video.currentTime = 0;
+		video.playbackRate = 1;
+	}
+	else {
+		video.currentTime = newTime;
+	}
+	console.log("Current location is " + video.currentTime + " seconds");
 } 
 
 function mute() { 
-  	
-  		console.log("Unmuted");
-  	
-      	console.log("Muted");
+  	if (video.muted === false) {
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+		console.log("Muted");
+	}
+	else {
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+		console.log("Unmuted");
+	}
 }
 
 function changeVolume() {
-;	console.log("Volume is ");
+	volumeSlider.value = 100;
+	volumeSlider.oninput = function () {
+		video.volume = this.value / 100;
+		dispVolume = this.value + "%";
+		document.querySelector("#volume").innerHTML = dispVolume;
+		console.log("Volume is " + dispVolume);
+	}
 }
        
-
 function gray() { 
-
-	console.log("In grayscale")
+	video.classList.add("grayscale");
+	console.log("In grayscale");
 }
 
 function color() {
-
-	console.log("In color") 
+	video.classList.remove("grayscale");
+	console.log("In color");
 }
